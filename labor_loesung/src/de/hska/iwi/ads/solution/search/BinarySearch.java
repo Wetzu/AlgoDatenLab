ee7033295120a57a8d080b29084b35836ee2a2d4
Wetzu <johannes@johannes-essig.de>
2018-10-17 13:46:00 +0200

    solved Binary Search

--- labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearch.java
+++ labor_loesung/src/de/hska/iwi/ads/solution/search/BinarySearch.java
@@ -1,14 +1,33 @@
 package de.hska.iwi.ads.solution.search;
 
-import de.hska.iwi.ads.search.Search;
-
 public class BinarySearch implements de.hska.iwi.ads.search.Search {
 
     @Override
     public int search(Comparable[] a, Comparable key) {
-        return 0;
+        int index = -1;
+        if (key.compareTo(a[0]) < 0) {
+            index = -1;
+        } else if (key.compareTo(a[a.length - 1]) > 0) {
+            index = a.length;
+        } else {
+            boolean found = false;
+            for (int i = 0; i <= a.length - 1 & !found; i++) {
+                if (a[i] == key) {
+                    index = i;
+                    found = true;
+                }
+            }
+            if (!found) {
+                for (int i = 0; i <= a.length - 1 & !found; i++) {
+                    if (key.compareTo(a[i]) > 0 && key.compareTo(a[i + 1]) < 0) {
+                        index = i + 1;
+                        found = true;
+                    }
+                }
+            }
+        }
+        return index;
     }
-
     @Override
 
     public int search(Comparable[] a, Comparable key, int left, int right) {
@@ -21,24 +40,19 @@ public class BinarySearch implements de.hska.iwi.ads.search.Search {
         }
         else {
             boolean found = false;
-            for (int i = left; i <= right; i++) {
+            for (int i = left; i <= right & !found; i++) {
                 if (a[i] == key) {
                     index = i;
                     found = true;
                 }
             }
-            if(!found){
-                for(int i = left; i <= right; i++) {
-                    if (key.compareTo(a[i - 1]) < 0 & key.compareTo(a[i]) > 0){
-                        index = i;
-                    }
+            for(int i = left; i <= right & !found; i++) {
+                if (key.compareTo(a[i]) > 0 && key.compareTo(a[i + 1]) < 0) {
+                    index = i + 1;
+                    found = true;
                 }
             }
         }
         return index;
-    }
-
-    public void main(String[] args){
-        int[] a = {1, 3, 5, 6, 7, 9};
     }
 }
